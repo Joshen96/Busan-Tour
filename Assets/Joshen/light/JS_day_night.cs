8cb3fece92da8f083aa11bcc40dc49a0532c7585
Joshen96 <asdzxc250@naver.com>
2023-06-27 09:51:30 +0900

    TMP 추가 밤낮테스트

---
+++ Assets/Joshen/light/JS_day_night.cs
@@ -0,0 +1,38 @@
+ using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class JS_day_night : MonoBehaviour
+{
+    public Material night = null;
+    public Material day = null;
+    public GameObject dLight;
+    [SerializeField]
+    private GameObject obj_Light;
+
+    public Color dayfog;
+    public Color nightfog;
+    // Start is called before the first frame update
+   
+
+
+    public void dayOn()
+    {
+        RenderSettings.skybox = day;
+        dLight.GetComponent<Light>().enabled = true;
+        obj_Light.SetActive(false);
+        RenderSettings.ambientLight = dayfog;
+        RenderSettings.fogDensity = 0;
+
+    }
+    public void nightOn()
+    {
+        RenderSettings.skybox = night;
+        dLight.GetComponent<Light>().enabled = false;
+        obj_Light.SetActive(true);
+        RenderSettings.ambientLight = nightfog;
+        RenderSettings.fogDensity = 0.001f;
+    }
+    
+}
