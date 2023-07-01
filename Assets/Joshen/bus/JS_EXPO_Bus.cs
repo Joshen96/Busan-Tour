f42523b0da1bd0d3f00270cd639d632c84a41d56
Joshen96 <asdzxc250@naver.com>
2023-07-01 22:10:09 +0900

    expo버스구현

--- Assets/Joshen/bus/JS_EXPO_Bus.cs
+++ Assets/Joshen/bus/JS_EXPO_Bus.cs
@@ -3,75 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JS_EXPO_Bus : MonoBehaviour
-{
+{/*
     [SerializeField]
     private GameObject bus;
+
     [SerializeField]
     private Transform player;
-    public Transform[] paths;
-    [SerializeField]
-    private float moveTime;
 
-    public float current;
-    public float percent;
+    public Transform[] paths;
+
     [SerializeField]
-    AnimationCurve moveCurve;
+    private float moveTime=0f;
+    
+    public float scrPlayDist = 8f;
+
 
     public int cnt = 0;
     private void Awake()
     {
+        bus.transform.position = paths[0].transform.position;
         
-        StartCoroutine(nameof(MoveTo));
     }
     private void Update()
     {
-        //turnBus(paths[cnt+1]);
+        moveTime = bus.gameObject.GetComponentInChildren<JS_bus_speed>().moveTime;
+
+        float dist = CalcDistanceWithTarget();
+
+        if(dist < scrPlayDist)
+        {
+            
+        }
+
+
+    }
+
+    private float CalcDistanceWithTarget()
+    {
+        Vector3 dirToTarget =
+            player.position - bus.transform.position;
+        float dist = dirToTarget.magnitude;
+
+        dist = Vector3.Distance(
+            player.position, bus.transform.position);
+
+        return dist;
     }
 
 
-    private IEnumerator MoveTo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void movetonextpos(Transform nowpath, Transform nextpath)
     {
-        bus.transform.position = paths[0].transform.position;
+        StartCoroutine(MoveTo(nowpath,nextpath));
+    }
 
+    private IEnumerator MoveTo(Transform nowpath,Transform nextpath)
+    {
 
+         //½Ã
 
-        while (true)
-        {
-            current = 0f;
-            percent = 0f;
-            while (percent < 1)
-            {
-
-                current += Time.deltaTime;
-                percent = current / moveTime;
-
-                bus.transform.position = Vector3.Lerp(paths[cnt].transform.position, paths[cnt + 1].position, moveCurve.Evaluate(percent));
-                if (percent > 0.8)
-                { 
-                    if(cnt==4)
-                    {
-                        cnt = 0;
-                    }
-                    turnBus(paths[cnt + 2]);
-                    
-                }
-
-                yield return null;
-
-            }
-            cnt++;
-            if (cnt >= paths.Length-1)
-            {
-                cnt = 0;
-            }
-
-        }
+        
         
     }
     public void turnBus(Transform _nextpath)
     {
         Vector3 dir = _nextpath.transform.position - bus.transform.position;
-        bus.gameObject.transform.rotation = Quaternion.Lerp(bus.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 5f);
+        bus.gameObject.transform.rotation = Quaternion.Lerp(bus.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 10f);
 
-    }
+    }*/
 }
