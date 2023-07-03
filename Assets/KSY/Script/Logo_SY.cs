933d4628bcd1be159ae34d9a7fffdb005541003c
WinterDotori <sying14@naver.com>
2023-07-03 12:59:25 +0900

    어방로고 프리펩(+)

---
+++ Assets/KSY/Script/Logo_SY.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logo_SY : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer logoRenderer = null;
+
+    private void Start()
+    {
+        if (!logoRenderer) logoRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == GameObject.FindGameObjectWithTag("Player"))  FadeOut();
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOut(3f));
+    }
+    IEnumerator FadeOut(float fadeOutTime)
+    {
+
+        Color tempColor = logoRenderer.color;
+        while (tempColor.a > 0f)
+        {
+            tempColor.a -= Time.deltaTime / fadeOutTime;
+            logoRenderer.color = tempColor;
+
+            if (tempColor.a <= 0f) tempColor.a = 0f;
+
+            yield return null;
+        }
+        logoRenderer.color = tempColor;
+    }
+}
